b528cb69069c8359efe4085ba2e4ae2b34892a50
gongkangjia <gongkangjia@gmail.com>
2021-05-14 01:46:04 +0800

    update .gitignore

--- pyioapi/__init__.py
+++ pyioapi/__init__.py
@@ -7,4 +7,4 @@
 
 from .read_girddesc import GRIDDESC
 
-__version__ == "0.1"
+__version__ == "0.1.1"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyioapi',
-    version=0.1,
+    version="0.1.1",
     description=(
         'The Python library provides read,write IOAPI-like netCDF file in CMAQ'
     ),
